b1e528ce4328ac5644d7355a6e7cf557716c3440
Sandesh <dsandesh263@gmail.com>
2024-08-14 12:26:13 +0545

    Fixed issues

--- nepse.py
+++ nepse.py
@@ -9,6 +9,8 @@ def speak_index(value):
     engine = tt.init()
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
+    rate = engine.getProperty('rate')  
+    engine.setProperty('rate', rate - 50)  
     engine.say(value)
     engine.runAndWait()
     engine.stop()
@@ -34,12 +36,13 @@ def detect_index_change():
         while True:
             current_data = fetch_index()[0][0]
             per_change = fetch_index()[0][1]
-            if previous_data is not None and current_data !=    previous_data:
-                speak_index(f"Change in Stock Index Detected by {per_change} percentage which now becomes {current_data}which means the market has been {'decreased' if current_data<previous_data else 'increased'}")
+            if previous_data is not None and current_data != previous_data:
+                speak_index(f"Change in Stock Index Detected by {per_change} percentage which now becomes {current_data} which means the market has been {'decreased' if per_change<0 else 'increased'}")
                 previous_data = current_data
             else:
-                speak_index(f"No changes detected! Index is still {current_data}")
-            time.sleep(3)
+                print("No changes detected!")
+                previous_data = current_data
+            time.sleep(1)
     except Exception as e:
         print(f"API Status Error: {e}")
 detect_index_change()
